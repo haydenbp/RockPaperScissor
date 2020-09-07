eae5b617646efa665eff216a05a96410cc485d66
haydenbp <32523980+haydenbp@users.noreply.github.com>
2020-09-07 13:05:29 -0400

    Update RPS.js

--- RPS.js
+++ RPS.js
@@ -1,2 +1,25 @@
-let compDiceroll = Math.floor(Math.random() * 6) + 1;
-console.log(compDiceroll);
+let computerPlay = Math.floor(Math.random() * 3) + 1;
+
+console.log(computerPlay);
+
+switch (computerPlay) {
+    case 1:
+        computerPlay = "Rock"
+    break;
+
+    case 2:
+        computerPlay = "Paper"
+
+    break;
+    
+    case 3:
+        computerPlay = "Scissor"
+
+    break;
+}
+
+console.log(computerPlay);
+
+let playerChoice = window.prompt("Rock, Paper, or Scissor?");
+
+console.log(`Player chose ${playerChoice}`);
